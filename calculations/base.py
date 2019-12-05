5d41738f397e2cf2daf22c258b075e4eab3506b6
wmkusters <wmkusters@me.com>
2019-12-04 22:35:00 -0500

    added distcalc documentation, script writes results

--- calculations/base.py
+++ calculations/base.py
@@ -20,11 +20,47 @@ class BaseCalc:
 
 
 class DistCalc(BaseCalc):
+    """
+    Base class for building dataframes mapping
+    bins to distances away from certain features
+    """
+
     def __init__(self, feature_df, binned_crime_df):
+        """
+        parameters:
+            feature_df: preprocessed PANDAS (not geopandas) dataframe of a feature
+            binned_crime_df: preproccessed binned crime pandas dataframe 
+        returns:
+            class instance
+
+        Pandas dataframes are used as data is read from a .csv, not from a .shp, so no
+        geometry column is set.
+        """
         BaseCalc.__init__(self, feature_df, binned_crime_df)
 
     def calculation(self, subgroup_list, feature_function):
+        """
+        parameters:
+            subgroup list: list of subgroups to independently calculate/return
+            feature function: function used on feature for calculation
+        returns:
+            list of resulting dataframes
+
+         This method performs a distance feature calculation, calculating distances 
+         of each bin to some feature with a location, i.e. bins to hospitals. With 
+         that list of distances, it then maps each bin to a function performed over 
+         those lists (feature_function parameter). An example function would be
+         summing the inverse squares of the distances from bin centroid to each feature.
+        """
+
         def haversine(coord_tuple):
+            """
+            parameters:
+                coord_tuple: string, contains (coord 1) and (coord 2) in tuple format
+                             concatenated together
+            returns:
+                haversine distance between coord 1 and coord 2
+            """
             (lon1, lat1, lon2, lat2) = coord_tuple
             lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
             dlon = lon2 - lon1
@@ -34,7 +70,12 @@ class DistCalc(BaseCalc):
             r = 3956  # Radius of earth in miles. Use 6371 for kilometers
             return c * r
 
+        # Get list of unique bin ids for distance mapping
         bin_ids = pd.unique(self.binned_crime_df.bin_id)
+
+        # Iterate through unique bins, calculating list of distances
+        # from that bin's centroid to each feature's coordinates,
+        # map bin_id to variable function of that distance list
         bin_distances = {}
         for bin_id in bin_ids:
             cent_coords = wkt.loads(
@@ -46,14 +87,16 @@ class DistCalc(BaseCalc):
             distances = []
             for point in self.feature_df.geometry:
                 point = wkt.loads(point)
-                hosp_coords = (point.x, point.y)
-                distances.append(haversine(cent_coords + hosp_coords))
+                feat_coords = (point.x, point.y)
+                distances.append(haversine(cent_coords + feat_coords))
             bin_distances[bin_id] = feature_function(distances)
 
+        # Add the result to the dataframe as a column
         self.binned_crime_df["feature"] = self.binned_crime_df.apply(
             lambda row: bin_distances[row["bin_id"]], axis=1
         )
 
+        # Return results filtered for each subgroup of crime
         results = []
         for subgroup in subgroup_list:
             crimes = subgroups[subgroup]
