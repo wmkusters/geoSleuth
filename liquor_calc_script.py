28117931b7f14ee21c7bd6dc7035f1ede8865b8a
wmkusters <wmkusters@me.com>
2019-12-07 22:54:40 -0500

    Moved grouper to calcs, to file

--- liquor_calc_script.py
+++ liquor_calc_script.py
@@ -6,15 +6,9 @@ from calculations.base import DiscreteCalc
 def main():
     feature_df = gpd.read_file("data/liquor_licenses_clean/liquor_licenses_clean.shp")
     binned_crimes = pd.read_csv("crimes_in_bins.csv")
-    calculator = DiscreteCalc(feature_df, binned_crimes)
+    calculator = DiscreteCalc(feature_df, binned_crimes, "Liquor")
     subgroups = ["Violent Crime", "Theft Crime"]
-    results = calculator.calculation(subgroups)
-    for result, subgroup in zip(results, subgroups):
-        # print(result[["bin_id", "OBJECTID", "OCCURRED_ON_DATE", "OFFENSE_CODE_GROUP", "crime_point"]].head())
-        result.to_csv(
-            "Liquor_Results/" + subgroup.replace(" ", "") + ".csv"
-        )
-
+    results = calculator.calculation(subgroups, group=True, to_file=True)
 
 if __name__ == "__main__":
     main()
