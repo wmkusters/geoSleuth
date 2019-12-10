8ee75073167f3e67536e1b81ef71e32685b8592a
wmkusters <wmkusters@me.com>
2019-12-10 00:22:06 -0500

    hospital calc script

--- hospital_dist_calc_script.py
+++ hospital_dist_calc_script.py
@@ -1,15 +1,25 @@
 from calculations.base import DistCalc
+from preprocessors.base import preprocess_hospital
+from analysis.base import BaseAnalyzer
 import pandas as pd
 import geopandas as gpd
 
+def inverse_square_sum(distances):
+    result = 0
+    for distance in distances:
+        result += 1/(distance*2)
+    return result
 
 def main():
-    feature_df = pd.read_csv("data/hospital_localtions_clean.csv")
-    binned_crimes = pd.read_csv("crimes_in_bins.csv")
-    calculator = DistCalc(feature_df, binned_crimes, feature_name="Hospital")
+    feature_df = preprocess_hospital("data/hospital-locations.csv")
+    binned_crimes = pd.read_csv("data/crimes_in_bins.csv")
+    calculator = DistCalc(feature_df, binned_crimes, feature_name="Hospital_InvSq")
     subgroups = ["Violent Crime", "Theft Crime"]
-    results = calculator.calculation(subgroups, feature_function=min, group=True, to_file=True)
-    
+    results = calculator.calculation(subgroups, feature_function=inverse_square_sum, group=True, to_file=False)
+    print(results.head())
+    raise SystemError(0)
+    analyzer = BaseAnalyzer(results)
+    BaseAnalyzer.linear_model(plot=True)
 
 
 if __name__ == "__main__":
